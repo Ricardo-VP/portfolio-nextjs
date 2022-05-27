2ffca3a1de362d02515ba9daff3ae4a5c100f250
Ricardo-VP <ricardo.vacape@gmail.com>
2022-05-26 22:25:03 -0500

    Adding spinner when changing routes ✌️

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,38 @@
 import Navbar from "./Navbar";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import nprogress from "nprogress";
 
-const Layout = ({ children }) => (
-  <>
-    <Navbar />
-    <main className="container py-4">{children}</main>
+const Layout = ({ children }) => {
+  const router = useRouter();
 
-    <footer className="bg-dark text-light text-center">
-      <div className="container p-4">
-        <h1>&copy; Ricardo Vaca</h1>
-        <p>2021 - {new Date().getFullYear()}</p>
-        <p>All rights reserved</p>
-      </div>
-    </footer>
-  </>
-);
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      console.log(url);
+      nprogress.start();
+    };
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    router.events.on("routeChangeComplete", () => nprogress.done());
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <main className="container py-4">{children}</main>
+
+      <footer className="bg-dark text-light text-center">
+        <div className="container p-4">
+          <h1>&copy; Ricardo Vaca</h1>
+          <p>2021 - {new Date().getFullYear()}</p>
+          <p>All rights reserved</p>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default Layout;
--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,13 @@ export default function Document() {
       <Head>
         <title>My Portfolio</title>
         <meta name="Ricardo Vaca Portfolio" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"
+          integrity="sha512-DanfxWBasQtq+RtkNAEDTdX4Q6BPCJQ/kexi/RftcP0BcA4NIJPSi7i31Vl+Yl5OCfgZkdJmCqz+byTOIIRboQ=="
+          crossOrigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
       </Head>
       <body>
         <Main />
